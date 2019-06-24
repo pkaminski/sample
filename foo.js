6e4903500997c1790f8c360099226e25218ee741
Piotr Kaminski <piotr@ideanest.com>
2019-06-23 22:45:50 -0700

    Update foo.js

--- foo.js
+++ foo.js
@@ -1,6 +1,6 @@
 function() {
-	var a = 'Hello'.replace(/H/, 'Z');
+	var a = 'Hello'.replace(/H/, 'X');
 	// tab indent
 	return 'quxxxer!';
-	// trailing spaces
+	// trailing spaces   
 }  // no newlinex
